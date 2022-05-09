7ca742eaa4289f2e10b0aa50d99cdae79985698d
AgustinSanchez29 <39387791+AgustinSanchez29@users.noreply.github.com>
2022-05-09 13:05:26 -0500

    fin de la seccion 5

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent{
+
+  nuevo:Personaje={
+    nombre:'',
+    poder:0
+  }
+
+  constructor(){
+  }//se inyecta el servicio por constructor
+   
+}
